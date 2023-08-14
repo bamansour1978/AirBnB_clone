23774be0493407c23f62c4771c793d1485f807da
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-08-14 03:14:40 +0200

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines unittests for models/place.py.
+"""Unittests for models/place.py.
 
 Unittest classes:
-    TestPlace_instantiation
-    TestPlace_save
-    TestPlace_to_dict
+    Test_instantiation
+    Test_save
+    Test_to_dict
 """
 import os
 import models
